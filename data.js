aa3d66a69f2b972ab6995bbe4992154bb74bdd29
sathyatn2004 <sathyatn2004@gmail.com>
2025-04-20 22:15:16 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+const main = {
+  name: 'sathya TN',
+  mail: 'sathyatn2004@gmail.com',
+  img: 'C:\Users\sathy\Downloads\ert.jpg',
+  role: ['Full Stack Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+   
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/sathya-tn/'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/sathya-tn/'
+    },
+   
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'mail@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' }
+  ]
+};
